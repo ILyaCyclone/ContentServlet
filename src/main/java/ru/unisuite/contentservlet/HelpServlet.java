e1ddbd35813de249f7fa2e9abdcfb9ff13169e15
Adusya <druna007@rambler.ru>
2019-11-05 18:36:24 +0300

    edit hepl page

--- src/main/java/ru/unisuite/contentservlet/HelpServlet.java
+++ src/main/java/ru/unisuite/contentservlet/HelpServlet.java
@@ -1,16 +1,15 @@
 package ru.unisuite.contentservlet;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-
+import java.io.OutputStream;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.common.io.ByteStreams;
 
 @WebServlet(name = "HelpServlet", urlPatterns = { "/help" })
 public class HelpServlet extends HttpServlet {
@@ -25,14 +24,10 @@ public class HelpServlet extends HttpServlet {
 
 	private void respondWithHelp(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		try (InputStream is = getServletContext().getResourceAsStream(HELP_FILE);
-				InputStreamReader isr = new InputStreamReader(is);
-				BufferedReader reader = new BufferedReader(isr);
-				PrintWriter writer = response.getWriter()) {
-			String text;
-			while ((text = reader.readLine()) != null) {
-				writer.println(text);
-			}
+		try (InputStream inputStream = getServletContext().getResourceAsStream(HELP_FILE);
+				OutputStream output = response.getOutputStream()) {
+			ByteStreams.copy(inputStream, output);
+			output.flush();
 		}
 	}
 }
