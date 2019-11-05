d104fad02327e339a6ded8454ca3d9c551ea7909
Adusya <druna007@rambler.ru>
2019-11-05 18:46:20 +0300

    change respondWithHelp method

--- src/main/java/ru/unisuite/contentservlet/HelpServlet.java
+++ src/main/java/ru/unisuite/contentservlet/HelpServlet.java
@@ -1,15 +1,15 @@
 package ru.unisuite.contentservlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.common.io.ByteStreams;
 
 @WebServlet(name = "HelpServlet", urlPatterns = { "/help" })
 public class HelpServlet extends HttpServlet {
@@ -24,10 +24,14 @@ public class HelpServlet extends HttpServlet {
 
 	private void respondWithHelp(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		try (InputStream inputStream = getServletContext().getResourceAsStream(HELP_FILE);
-				OutputStream output = response.getOutputStream()) {
-			ByteStreams.copy(inputStream, output);
-			output.flush();
+		try (InputStream is = getServletContext().getResourceAsStream(HELP_FILE);
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader reader = new BufferedReader(isr);
+				PrintWriter writer = response.getWriter()) {
+			String text;
+			while ((text = reader.readLine()) != null) {
+				writer.println(text);
+			}
 		}
 	}
 }
